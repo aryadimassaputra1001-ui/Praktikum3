b6e942b788ad804c774eab3e3a1e586f2b9e45cb
aryadimassaputra1001-ui <aryadimassaputra1001@gmail.com>
2025-10-17 09:40:35 +0700

    Update praktikum3.py

--- praktikum3.py
+++ praktikum3.py
@@ -1,1 +1,21 @@
-
+ def cari_terbesar(a, b, c):
+   """Mencari bilangan terbesar dari tiga input.
+   Args:
+   a: Bilangan pertama.
+   b: Bilangan kedua.
+   c: Bilangan ketiga.
+ Return:
+  Bilangan terbesar dari a, b, c.
+ """
+ if a >= b and a >= c:
+   return a
+ elif b >= a and b >= c:
+   return b
+ else:
+   return c
+# Contoh penggunaan
+bil1 = 10
+bil2 = 5
+bil3 = 20
+terbesar = cari_terbesar(bil1, bil2, bil3)
+print("Bilangan terbesar adalah:", terbesar) # Output: Bilangan terbesar adalah: 20
